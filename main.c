b4ea4244218f5e956f817c09bdafba1770f8b279
simon <simon@gmai.com>
2024-11-13 09:50:47 -0800

    first lecture

--- main.c
+++ main.c
@@ -1,1 +1,23 @@
-ECHO is on.
+#include<stdio.h>
+int main()
+{
+/*	int x =100;
+	int y =3;
+	char k='B';
+	float q = x/y;
+	double b = 2.33434;
+	
+	printf("%d\n",x);
+	printf("%d\n",y);
+	printf("%c\n",k);
+	printf("%f\n",q);
+	printf("%lf\n",b);*/
+	
+	int x=100;
+	x=2000;
+	printf("the value of x is %d",x);
+	
+	
+return 0;
+
+}
