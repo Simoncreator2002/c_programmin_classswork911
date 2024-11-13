4b52c60fe0ef6024d2543ca6934221ae881540a0
simon <simon@gmai.com>
2024-11-13 09:51:47 -0800

    first lecture

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
+#include<stdbool.h>
 int main()
 {
+	//first git session
 /*	int x =100;
 	int y =3;
 	char k='B';
@@ -11,13 +13,35 @@ int main()
 	printf("%d\n",y);
 	printf("%c\n",k);
 	printf("%f\n",q);
-	printf("%lf\n",b);*/
+	printf("%lf\n",b);
 	
 	int x=100;
 	x=2000;
 	printf("the value of x is %d",x);
 	
 	
+	int y= 10;
+	int k= 3;
+	printf("%d",(y%k));
+	int h=3;
+	h++;
+	printf("%d/n",h); 
+	
+	bool k = false;
+	printf("%d",k);
+	
+	
+	int m =200;
+	int n= 300;
+	if (m==n)
+	{
+		printf("these two variables are equal");
+		
+	}
+	else{
+		printf("the variables are not equaal");
+	}
+	*/
 return 0;
 
 }
